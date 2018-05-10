243de35fe81fa8a661e1fb6d1d1d0296b4efac79
vladimir <vladimir@way2cu.com>
2018-05-10 12:27:32 +0200

    Manifest file: Properly formated.

--- site/config.php
+++ site/config.php
@@ -17,7 +17,7 @@ define('DEBUG', 1);
 // define('SQL_DEBUG', 1);
 
 // site language configuration
-$available_languages = array('en', 'he');
+$available_languages = array('en');
 $default_language = 'en';
 
 // default session options
