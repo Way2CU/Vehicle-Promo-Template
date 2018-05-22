c33783eb20ea49095fac9d95de5d975c82394402
vladimir <vladimir@way2cu.com>
2018-05-22 16:18:49 +0200

    Removed link module call and accompained stuff, style fixes on both versions of site.

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -52,20 +52,13 @@ Site.on_load = function() {
 	if (Site.is_mobile()) {
 		Site.mobile_menu = new Caracal.MobileMenu();
 
-		var preview_tables = document.querySelector('div#vehicle_stats > article');
+		var vehicle_preview = document.querySelector('div#vehicle_stats > article');
 		var vehicle_overview = document.querySelector('div#vehicle_overview');
 
-		preview_tables.addEventListener('click', function(event) {
+		vehicle_preview.addEventListener('click', function(event) {
 			event.preventDefault();
 			vehicle_overview.classList.toggle('toggle_overview_visibility');
-			console.log("damn");
 		});
-		// for(var i=0; i < preview_tables.length; i++) {
-		// 	preview_tables[i].addEventListener('click', function(event) {
-		// 		event.preventDefault();
-		// 		vehicle_overview.classList.toggle('toggle_overview_visibility');
-		// 	});
-		// }
 	}
 };
 
